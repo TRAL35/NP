c57670711b5149e3d4d5efe7698bf7d1d877bf1e
TRAL35 <opraserone@gamil.com>
2021-11-16 21:25:33 +1100

    test commit

--- src/ReaderProfile.java
+++ src/ReaderProfile.java
@@ -1,5 +1,6 @@
 
-public class ReaderProfile {
+public class ReaderProfile 
+{
 	String name;
     String secondname;
     String grade;
@@ -10,52 +11,67 @@ public class ReaderProfile {
     {
         return grade;
     }
+
     public String getName() 
     {
         return name;
     }
+
     public String getPhone() 
     {
         return phone;
     }
+
     public String getSecondName() 
     {
         return secondname;
     }
+
     public int getTakenBooks() 
     {
         return takenBooks;
     }
+
     public void setGrade(String grade) 
     {
-        if(grade != null && !grade.isEmpty() && grade.length() == 1) {
+        if(grade != null && !grade.isEmpty() && grade.length() == 1)
+        {
             this.grade = grade;
         }
     }
+
     public void setName(String name) 
     {
-        if(name != null && !name.isEmpty()) {
+        if(name != null && !name.isEmpty()) 
+        {
             this.name = name;
         }
     }
+
     public void setPhone(String phone) 
     {
-        if(phone != null && phone.length() == 10) {
+        if(phone != null && phone.length() == 10) 
+        {
             this.phone = phone;
     	}
     }
+
     public void setSecondName(String secondname) 
     {
-        if(secondname != null && !secondname.isEmpty()) {
+        if(secondname != null && !secondname.isEmpty()) 
+        {
             this.secondname = secondname;
         }
     }
+
     public void setTakenBooks(int takenBooks) 
     {
-        if(takenBooks > -1) {
+        if(takenBooks > -1) 
+        {
             this.takenBooks = takenBooks;
         }
     }
+
     public ReaderProfile(String name, String secondname, String grade, String phone, int takenBooks)
     {
         setName(name);
@@ -64,6 +80,7 @@ public class ReaderProfile {
         setPhone(phone);
         setTakenBooks(takenBooks);
     }
+
     public ReaderProfile(String name, String secondname, String grade, String phone)
     {
         setName(name);
@@ -71,6 +88,7 @@ public class ReaderProfile {
         setGrade(grade);
         setPhone(phone);
     }
+
     public String toString() 
     {
         return String.format("%s, %s, %s, %s, %d", getName(), getSecondName(), getGrade(), getPhone(), getTakenBooks());
